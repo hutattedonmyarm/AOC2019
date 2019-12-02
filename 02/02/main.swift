f56e95f89e1456af94e822792d3db5a6e524c9c8
aymm <max.nuding@icloud.com>
2019-12-02 07:40:54 +0100

    Brute force of day 2 Part 2

--- 02/02/main.swift
+++ 02/02/main.swift
@@ -4,25 +4,53 @@ guard let fileContents = try? String(contentsOfFile: "input.txt") else {
     fatalError("Cannot open input file")
 }
 
-var input = fileContents.split(separator: ",").map {Int($0.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines))!}
+var originalInput = fileContents.split(separator: ",").map {Int($0.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines))!}
 
-input[1] = 12
-input[2] = 2
+let desiredOutput = 19690720
+var doBreak = false
 
-var index = 0
-var opCode = input[index]
-repeat {
-    let value1 = input[input[index+1]]
-    let value2 = input[input[index+2]]
-    var result = 0
-    if opCode == 1 {
-        result = value1 + value2
-    } else if opCode == 2 {
-        result = value1 * value2
+var input = originalInput
+for noun in 0...99 {
+    for verb in 0...99 {
+        input = originalInput
+        input[1] = noun
+        input[2] = verb
+
+        var index = 0
+        var opCode = input[index]
+        
+        repeat {
+            let adress1 = input[index+1]
+            let adress2 = input[index+2]
+            
+            
+            let tooLarge = max(adress1, adress2) - input.count
+            if (tooLarge > 0) {
+                input.append(contentsOf: Array(repeating: 0, count: tooLarge))
+            }
+            
+            let value1 = input[adress1]
+            let value2 = input[adress2]
+            var result = 0
+            if opCode == 1 {
+                result = value1 + value2
+            } else if opCode == 2 {
+                result = value1 * value2
+            }
+            input[input[index+3]] = result
+            index += 4
+            opCode = input[index]
+        } while opCode != 99
+        if input[0] == desiredOutput {
+            print(noun)
+            print(verb)
+            print(100*noun + verb)
+            break
+        }
     }
-    input[input[index+3]] = result
-    index += 4
-    opCode = input[index]
-} while opCode != 99
+    if input[0] == desiredOutput {
+        break
+    }
+}
 
 print(input[0])
